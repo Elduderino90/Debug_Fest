22ed5bc6d9b92628b572db390bdf170f80bbf33f
mohammed_1057713 <1057713@my.scccd.edu>
2025-10-26 19:41:17 -0700

    func2 fix messages test

--- main.cc
+++ main.cc
@@ -73,7 +73,7 @@ int function2() {
 	int sum = 0;
 
 	srand(seed); // took out for loop cuz needs to generate once not 20 times
-	for (int i = 0; i <= 20; i++) {
+	for (int i = 0; i < 20; i++) {
 		
 		sum += (rand() % 6) + 1;
 	}
